8e7792bc7a0741014fe21b21614d754e29d0b9e0
Mārtiņš Dīcis <dicis.martins@gmail.com>
2019-07-13 00:18:53 +0300

    Add project files.

---
+++ DustCollageMaker/MainPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Toolkit.Uwp.UI.Controls;
+using System;
+using System.Collections.ObjectModel;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace DustCollageMaker
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private Carousel selectedItem = new Carousel();
+
+        private ObservableCollection<Image> images = new ObservableCollection<Image>();
+
+        public ObservableCollection<Image> Images { get { return this.images; } }
+        
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            var picker = new Windows.Storage.Pickers.FileOpenPicker
+            {
+                ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail,
+                SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.PicturesLibrary
+            };
+            picker.FileTypeFilter.Add(".jpg");
+            picker.FileTypeFilter.Add(".jpeg");
+            picker.FileTypeFilter.Add(".png");
+
+            Windows.Storage.StorageFile file = await picker.PickSingleFileAsync();
+            if (file != null)
+            {
+                // Ensure the stream is disposed once the image is loaded
+                using (IRandomAccessStream fileStream = await file.OpenAsync(Windows.Storage.FileAccessMode.Read))
+                {
+                    // Set the image source to the selected bitmap
+                    BitmapImage bitmapImage = new BitmapImage();
+                    await bitmapImage.SetSourceAsync(fileStream);
+                    this.images.Add(new Image { Source = bitmapImage });
+                }
+
+                var sdfsdf = "Picked photo: " + file.Name;
+            }
+            else
+            {
+                var dfgdf = "Operation cancelled.";
+            }
+        }
+
+        private void CarouselControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            selectedItem = sender as Carousel;
+            imageResized.Source = images[selectedItem.SelectedIndex].Source;
+        }
+    }
+}
